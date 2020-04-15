330313677fae16531afbc85945b0259c07e53bcc
Szymon Osinski <szymek16s@gmail.com>
2020-04-15 18:50:42 +0200

    Delete all the unnecessary comments and function

--- src/gra_odbijacz/Gra_odbijacz.java
+++ src/gra_odbijacz/Gra_odbijacz.java
@@ -25,13 +25,7 @@ public class Gra_odbijacz extends JFrame {
         
         this.setBounds(500, 500, 500, 500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-//        for(int i= 0; i<kwadraciki.length;i++)
-//        {
-//        kwadraciki[i] = new Kwadracik("Kwadrat Nr");
-//        animacja.add(kwadraciki[i]);
-//        }
-        
+         
         this.getContentPane().add(panel_Przycisków, BorderLayout.SOUTH);
         this.getContentPane().add(animacja);
         
@@ -140,17 +134,9 @@ public class Gra_odbijacz extends JFrame {
           
          if( ((Piłka)listaPiłek.get(j)).y == (((Kwadracik)listaKwadratów.get(i)).y + ((Kwadracik)listaKwadratów.get(i)).yKwadratu) && (((Piłka)listaPiłek.get(j)).x + ((Piłka)listaPiłek.get(j)).xKropelki) >= ((Kwadracik)listaKwadratów.get(i)).x && ((Piłka)listaPiłek.get(j)).x <= (((Kwadracik)listaKwadratów.get(i)).x + ((Kwadracik)listaKwadratów.get(i)).xKwadratu)) 
          {
-             //System.out.println("dotknęlam górnej dolnej cześci");011
          Gra_odbijacz.WYNIK ++;
          ((Piłka)listaPiłek.get(j)).odbiciePiłkiodKwadratuPoY();
          listaKwadratów.remove(listaKwadratów.get(i));
-         
-         System.out.println("Długość listy kwadratów "+": "+listaKwadratów.size());
-        System.out.println("Pozyccja kwadratu zbijanego"+": "+i);
-             System.out.println("Instrukcja 1");
-             //System.out.println("Piłka dotknęła kwadraiciku" + " "+ "Kwadracik y-yKwadaru wynik "+": "+(kwadraciki[i].y - kwadraciki[i].yKwadratu) + " "+ " Y Piłki" +": "+ ((Piłka)listaPiłek.get(0)).y);
-             //System.out.println("Piłka dotknęła kwadraiciku" + " "+ "Kwadracik x "+": "+(kwadraciki[i].x) + " "+ " x Piłki" +": "+ (((Piłka)listaPiłek.get(0)).x + ((Piłka)listaPiłek.get(0)).xKropelki));
-           //  System.out.println("Długość listy kwadratów "+": "+listaKwadratów.size());
          }
          
          else if((((Piłka)listaPiłek.get(j)).y+ ((Piłka)listaPiłek.get(j)).yKropelki) == ((Kwadracik)listaKwadratów.get(i)).y && (((Piłka)listaPiłek.get(j)).x + ((Piłka)listaPiłek.get(j)).xKropelki) >= ((Kwadracik)listaKwadratów.get(i)).x && ((Piłka)listaPiłek.get(j)).x <= (((Kwadracik)listaKwadratów.get(i)).x + ((Kwadracik)listaKwadratów.get(i)).xKwadratu))
@@ -187,16 +173,13 @@ public class Gra_odbijacz extends JFrame {
           }
          }
     }
-    
-        //System.out.println("Wynik:" + Gra_odbijacz.WYNIK);
-     
+
     }
     
     public void addBelka()
     {
      
     listaBelek.add(belkaa);
-    //System.out.println("Długość belek: "+listaBelek.size());
     ((Belka)listaBelek.get(0)).przypiszY(ten);
     watekBelki = new Thread(new BelkaRunnable((Belka)listaBelek.get(listaBelek.size()-1)));
     watekBelki.start();
@@ -212,7 +195,7 @@ public class Gra_odbijacz extends JFrame {
     kwadraciki[i] = new Kwadracik();
     listaKwadratów.add(kwadraciki[i]);
     }
-    //System.out.println(listaKwadratów.size());
+ 
     watekKwadratów = new Thread(new KwadratRunnable(kwadraciki));
     watekKwadratów.start();
     repaint();
@@ -225,26 +208,17 @@ public class Gra_odbijacz extends JFrame {
             
         for(int i =0; i <listaPiłek.size(); i++)
             {
-            //g.drawImage(Piłka.PIŁKA, ((Piłka)listaPiłek.get(i)).x, ((Piłka)listaPiłek.get(i)).y, null);
               g.drawImage(Piłka.PIŁKA, ((Piłka)listaPiłek.get(i)).x, ((Piłka)listaPiłek.get(i)).y, null);
-            // g.drawImage(Piłka.PIŁKA,(((Belka)listaBelek.get(i)).getX()+((Belka)listaBelek.get(i)).getXBelki())/2,(((Belka)listaBelek.get(i)).getY()-((Belka)listaBelek.get(i)).getYBelki()) , null);
-             //  System.out.println( "Y piłki" +((Piłka)listaPiłek.get(i)).y);
             }
         for(int a =0; a< listaBelek.size(); a++)
             {
             g.drawImage(Belka.BELKA,((Belka)listaBelek.get(a)).x,((Belka)listaBelek.get(a)).y, null);
-                //System.out.println("Wymalowałem Belke");
-                //System.out.println("Y Belki" + ": "+ ((Belka)listaBelek.get(a)).y);
-                //System.out.println("X Belki" + ": "+ ((Belka)listaBelek.get(a)).x);
             }
         for(int z =0; z< listaKwadratów.size();z++)
             {
             g.drawImage(Kwadracik.KWADRAT, ((Kwadracik)listaKwadratów.get(z)).x, ((Kwadracik)listaKwadratów.get(z)).y, null);
-              //   System.out.println("Długość listy kwadratów z metoda Paint "+": "+listaKwadratów.size());
-                  
-               // System.out.println("Wymalowałem kwadart nr:" +((Kwadracik)listaKwadratów.get(z)) +" "+ "O wymiarach:" +((Kwadracik)listaKwadratów.get(z)).x );
             }
-              //System.out.println("Kiedy zostałem wywołany");
+             
             
     }
     ArrayList listaKwadratów = new ArrayList();
@@ -302,13 +276,12 @@ public class Gra_odbijacz extends JFrame {
             {
                 while(!Thread.currentThread().isInterrupted())
                 {
-                 //System.out.println("Początek lini w meotdzie Piłka"); 
-                
+
                 piłka.odbijaniePiłki(ten, (Belka)listaBelek.get(listaBelek.size()-1), listaPiłek);
                 repaint();
                 czyPiłkaSięSpotkałaZKwadratem();
                 repaint();
-                //System.out.println("Koniec lini w meotdzie Piłka");
+           
                 try
                 {
                 Thread.sleep(Gra_odbijacz.PREDKOSC);
@@ -316,7 +289,7 @@ public class Gra_odbijacz extends JFrame {
                 catch (InterruptedException ex)
                 {
                     System.out.println(ex.getMessage());
-                    //Thread.interrupted();
+              
                     
                 }
                 
@@ -326,7 +299,7 @@ public class Gra_odbijacz extends JFrame {
                 
                 watekPiłki.interrupt();
                 
-                //dialog.setVisible(true);
+               
                 
                 wiadomoscZamknięcia();
                 
@@ -361,26 +334,12 @@ public class Gra_odbijacz extends JFrame {
             
                 for(int i =0; i <kwadraciki.length; i++)
                     {
-//                        System.out.println("Pętla górna tabela");
-                     //kwadrat[i].ustawKwadraty(kwadrat[i], ten);
-                     
-                        //(Kwadracik)listaKwadratów.get(i)).ustawKwadraty(kwadrat[i], ten);
-                        
                         ((Kwadracik)listaKwadratów.get(i)).ustawKwadraty3((Kwadracik)listaKwadratów.get(i), ten);
-//                     System.out.println(kwadrat[i].toString());
-//                     System.out.println("Pętla dolna tabela");
                 repaint();
                     }
                 
                 System.out.println("siemasssssssssssssssssssssss");
-                
-//                kwadrat[0].x = 10;
-//                kwadrat[1].x = 55;
-//                kwadrat[2].x = 100;
-//                kwadrat[3].x = 145;
-//                kwadrat[4].x = 190;
-                
-                   
+               
             } 
             
     }
