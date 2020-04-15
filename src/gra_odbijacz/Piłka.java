096f546426bc9600cc0130d6609b4121e72d670f
Szymon Osinski <szymek16s@gmail.com>
2020-04-15 20:20:59 +0200

    Improve readable of code in class Piłka.java

--- src/gra_odbijacz/Piłka.java
+++ src/gra_odbijacz/Piłka.java
@@ -31,81 +31,68 @@ public void przypiszPozycjePiłki(ArrayList listaBelek, Piłka piłka)
 {
     piłka.y = ((((Belka)listaBelek.get(listaBelek.size()-1)).y - piłka.yKropelki) -1);
     piłka.x = ((((Belka)listaBelek.get(listaBelek.size()-1)).x +(((Belka)listaBelek.get(listaBelek.size()-1)).xBelki)/2)); 
- 
-   
 }
 
 
 public void odbiciePiłkiodKwadratuPoY()
 {
-
-dy = -dy;
+    dy = -dy;
 }
 
 public void odbiciePiłkiodKwadartuPoX()
 {
-
-dx= -dx;
+    dx= -dx;
 }
 
 public void odbijaniePiłki(JPanel panel, Belka belka, ArrayList lista)
 {
-Rectangle układ = panel.getBounds();
+    Rectangle układ = panel.getBounds();
 
 
-x +=dx;
-y +=dy;
+    x +=dx;
+    y +=dy;
 
-if(y + yKropelki == układ.getMaxY()- belka.yBelki)
-{
-
-      if((x+xKropelki)>= belka.x && x<= (belka.xBelki/4)+belka.x)  
+    if(y + yKropelki == układ.getMaxY()- belka.yBelki)
     {
+        // Nadawanie piłce prędkości
+          if((x+xKropelki)>= belka.x && x<= (belka.xBelki/4)+belka.x)  
+          {
+            Gra_odbijacz.PREDKOSC=10;    
+            dy = -2*dy;
+          }
 
-    Gra_odbijacz.PREDKOSC=10;    
-    dy = -2*dy;
-  
-        System.out.println("1"+ dy);
+
+          else if(x+xKropelki >= ((belka.xBelki/4)+belka.x) && x<= ((belka.xBelki/2)+belka.x))
+          {
+            Gra_odbijacz.PREDKOSC=10;
+            dy = -2*dy;  
+          }
+
+          else if((x+xKropelki)>= (((belka.xBelki)/2)+belka.x)&& x<= (((belka.xBelki/4)*3)+belka.x))
+          {
+            Gra_odbijacz.PREDKOSC=10;
+            dy = -2*dy;
+          }
+
+          else if((x+xKropelki)>= (((belka.xBelki/4)*3)+belka.x) && x<= (belka.x+ belka.xBelki))
+          {
+            Gra_odbijacz.PREDKOSC=10;    
+            dy= -2*dy;
+          }
     }
     
-
-      else if(x+xKropelki >= ((belka.xBelki/4)+belka.x) && x<= ((belka.xBelki/2)+belka.x))
-   {
-    
-        Gra_odbijacz.PREDKOSC=10;
-        dy = -2*dy;
-       System.out.println("2"+ dy);
-    }
-    
-      else if((x+xKropelki)>= (((belka.xBelki)/2)+belka.x)&& x<= (((belka.xBelki/4)*3)+belka.x))
-    {
-
-    Gra_odbijacz.PREDKOSC=10;
-    dy = -2*dy;
-        System.out.println("3"+ dy);
-    }
-
-      else if((x+xKropelki)>= (((belka.xBelki/4)*3)+belka.x) && x<= (belka.x+ belka.xBelki))
-    {
-
-    Gra_odbijacz.PREDKOSC=10;    
-    dy= -2*dy;
-
-        System.out.println("4"+ dy);
-    }
-}
-else if(x + xKropelki >= układ.getMaxX())
-{
-dx = -dx;
-}
-else if(y < układ.getMinY())
-{
-dy = -dy;
-}
-else if(x < układ.getMinX())
-{
-dx = -dx;
-}
+            else if(x + xKropelki >= układ.getMaxX())
+            {
+            dx = -dx;
+            }
+            else if(y < układ.getMinY())
+            {
+            dy = -dy;
+            }
+            else if(x < układ.getMinX())
+            {
+            dx = -dx;
+            }
 }
 
 
