467330e72f71b43fcf147e53f8633835dae2e008
Szymon Osinski <szymek16s@gmail.com>
2020-04-15 18:54:12 +0200

    Delete all the unnecessary comments and function from all classes.

--- src/gra_odbijacz/Piłka.java
+++ src/gra_odbijacz/Piłka.java
@@ -32,15 +32,13 @@ public void przypiszPozycjePiłki(ArrayList listaBelek, Piłka piłka)
     piłka.y = ((((Belka)listaBelek.get(listaBelek.size()-1)).y - piłka.yKropelki) -1);
     piłka.x = ((((Belka)listaBelek.get(listaBelek.size()-1)).x +(((Belka)listaBelek.get(listaBelek.size()-1)).xBelki)/2)); 
  
-    //System.out.println("Y Piłki "+" :"+piłka.y);11
-    //System.out.println("X Piłki "+" :"+piłka.x);
+   
 }
 
 
 public void odbiciePiłkiodKwadratuPoY()
 {
-//dx = -dx;
-//dy=-1;
+
 dy = -dy;
 }
 
@@ -58,52 +56,41 @@ Rectangle układ = panel.getBounds();
 x +=dx;
 y +=dy;
 
-// System.out.println("x + xKropelki" + (x + xKropelki) );
-//    System.out.println("xBelki x2"+ ": "+(belka.x+ belka.xBelki)  );
 if(y + yKropelki == układ.getMaxY()- belka.yBelki)
 {
-//    if(x  <= (belka.x+ belka.xBelki) && (x + xKropelki) >= (belka.x)) //&& (y - yKropelki) <= (układ.getMaxY() - belka.yBelki) )
-//    {
-//    dy = -dy;
-//    dx =-dx;
-//    System.out.println("działa ");
-//    }
-    
-    //if((x+xKropelki)>= belka.x && x <= ((((belka.x + belka.xBelki)-belka.x)/4)))
+
       if((x+xKropelki)>= belka.x && x<= (belka.xBelki/4)+belka.x)  
     {
-    //dy=2;
+
     Gra_odbijacz.PREDKOSC=10;    
     dy = -2*dy;
-    //dx = -dx;
+  
         System.out.println("1"+ dy);
     }
     
-   // else if(x+xKropelki>= ((((belka.x + belka.xBelki)-belka.x)/4)*3) && x <= (((belka.x + belka.xBelki)-belka.x)/2))
+
       else if(x+xKropelki >= ((belka.xBelki/4)+belka.x) && x<= ((belka.xBelki/2)+belka.x))
    {
-        //dy= 1.5;
+    
         Gra_odbijacz.PREDKOSC=10;
         dy = -2*dy;
        System.out.println("2"+ dy);
     }
     
-    //else if(x+xKropelki>= (((belka.x + belka.xBelki)-belka.x)/2) && x<= ((((belka.x + belka.xBelki)-belka.x)/4)*3))
       else if((x+xKropelki)>= (((belka.xBelki)/2)+belka.x)&& x<= (((belka.xBelki/4)*3)+belka.x))
     {
-    //dy =1.5;
+
     Gra_odbijacz.PREDKOSC=10;
     dy = -2*dy;
         System.out.println("3"+ dy);
     }
-    
-   // else if(x+xKropelki>=((((belka.x + belka.xBelki)-belka.x)/4)*3) && x<= (belka.x +belka.xBelki) )
+
       else if((x+xKropelki)>= (((belka.xBelki/4)*3)+belka.x) && x<= (belka.x+ belka.xBelki))
     {
-    //dy=1;
+
     Gra_odbijacz.PREDKOSC=10;    
     dy= -2*dy;
-   // dx= -dx;
+
         System.out.println("4"+ dy);
     }
 }
